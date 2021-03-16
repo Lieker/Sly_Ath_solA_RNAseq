f117ddf06b645c69febc538b3f9ef48b4cca36d4
lieker <l.e.vlaar@uva.nl>
2021-03-16 18:24:51 +0100

    annotations fixed

--- Sly1/scripts/get_Slycopersicum_annotations.R
+++ Sly1/scripts/get_Slycopersicum_annotations.R
@@ -4,15 +4,27 @@ library("biomaRt")
 library(org.At.tair.db)
 library(stringr)
 
-get_Slycopersicum_annotations <- function(
+get_Slycopersicum_annotations <- function(attr = c("description",
+                                                   "athaliana_eg_homolog_ensembl_gene",
+                                                   "athaliana_eg_homolog_associated_gene_name",
+                                                   "external_gene_name",
+                                                   "athaliana_eg_homolog_perc_id") 
 ) {
   descriptions <- read.csv("inputs/ITAG4.1_descriptions.csv", stringsAsFactors = FALSE)
   descriptions$gene <- substr(descriptions$gene, 1, 16)
   
   go_terms <- read.csv("inputs/ITAG4.1_goterms.csv", stringsAsFactors = FALSE)
-  go_terms$gene <- substr(descriptions$gene, 1, 16)
+  go_terms$gene <- substr(go_terms$gene, 1, 16)
   
   annotations <- left_join(descriptions, go_terms, by = "gene")
+  annotations$gene_id_versionless <- substr(annotations$gene, 1, 14)
+  
+  source("scripts/get_Slycopersicum_annotations2.R")
+  
+  ensembl_annotations <- get_Slycopersicum_annotations2(attr = attr)
+  
+  annotations <- left_join(annotations, ensembl_annotations, by = "gene_id_versionless", suffix = c("_ITAG4.0", "_ITAG3.0"))
+  
   return(annotations)
 }
   
--- Sly1/scripts/get_annotated_DEGs.R
+++ Sly1/scripts/get_annotated_DEGs.R
@@ -15,17 +15,22 @@ get_annotated_DEGs <- function(counts_csv_file = "inputs/raw_counts.csv",
                                treatment2 = "yes_solA",
                                log2FC_threshold = 0,
                                padj_threshold = 0.05,
-                               name = "outputs/annotated_DEGslist.csv") {
-  
+                               name = "outputs/annotated_DEGslist.csv",
+                               attr = c("description",
+                                        "athaliana_eg_homolog_ensembl_gene",
+                                        "athaliana_eg_homolog_associated_gene_name",
+                                        "external_gene_name",
+                                        "athaliana_eg_homolog_perc_id")
+                               ) {
   res <- get_list_of_DEGs(counts_csv_file, 
-                        xp_design_csv_file, 
-                        plantpart, 
-                        ref_treatment, 
-                        treatment2, 
-                        log2FC_threshold, 
-                        padj_threshold) %>% rownames_to_column("gene")
-  
-  annotations <- get_Slycopersicum_annotations()
+                         xp_design_csv_file, 
+                         plantpart, 
+                         ref_treatment, 
+                         treatment2, 
+                         log2FC_threshold, 
+                         padj_threshold) %>% rownames_to_column("gene")
+   
+  annotations <- get_Slycopersicum_annotations(attr = attr)
   res_annotated <- left_join(res, annotations, by = "gene")
   write_delim(x = res_annotated,
               file = name,
