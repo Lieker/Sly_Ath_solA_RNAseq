134e6029b0506b86c1cf108990c5f7f277b784ea
lieker <l.e.vlaar@uva.nl>
2021-03-26 15:36:33 +0100

    adjusted pca function

--- Ath2/scripts/pca_function.R
+++ Ath2/scripts/pca_function.R
@@ -2,7 +2,16 @@ mypca <- function(x, center = T, scale = T){
   
   # remove columns containing only 0 values
   # not informative + cause svd() error
-  x_without_zero_columns <- x[vapply(x, function(z) length(unique(z)) > 1, logical(1L))]
+  x_without_zero_columns_matrix <- x[vapply(x,
+                                     function(z) length(unique(z)) > 1,
+                                     logical(1L))] %>% data.matrix(., rownames.force = T)
+  x_without_zero_columns_frame <- x[vapply(x,
+                                           function(z) length(unique(z)) > 1,
+                                           logical(1L))]
+  median_all_genes <- median(x_without_zero_columns_matrix)
+  median_per_gene <- apply(x_without_zero_columns_frame, 2, median)
+  x_without_zero_columns_frame_t <- x_without_zero_columns_frame %>% t() %>% as.data.frame()
+  x_without_zero_columns <- x_without_zero_columns_frame_t %>% filter(median_per_gene > median_all_genes) %>% t() %>% as.data.frame()
   
   # perform SVD
   SVD <- svd(scale(x_without_zero_columns, center = center, scale = scale))
