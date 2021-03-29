607b218894ec322033499f9291fc5c9e8c900ee3
lieker <l.e.vlaar@uva.nl>
2021-03-29 15:00:04 +0200

    added comparison of null test (LRT) with full test (Wald)

--- Ath2/scripts/get_annotated_DEGs.R
+++ Ath2/scripts/get_annotated_DEGs.R
@@ -4,9 +4,9 @@ library("biomaRt")
 library(clusterProfiler)
 library(org.At.tair.db)
 
+source("scripts/compare_Wald_vs_LRT.R")
 source("scripts/get_filtered_list_of_DEGs.R")
 source("scripts/get_Athaliana_annotations.R")
-source("scripts/DEG_function.R")
 
 get_annotated_DEGs <- function(counts_csv_file = "inputs/counts.csv",
                                xp_design_csv_file = "inputs/xp_design.csv",
@@ -22,23 +22,29 @@ get_annotated_DEGs <- function(counts_csv_file = "inputs/counts.csv",
                                         "description",
                                         "external_gene_name",
                                         "external_gene_source"),
-                               name = "outputs/annotated_DEGslist.csv") {
+                               name = "outputs/annotated_DEGslist.csv",
+                               method = "DEG") {
+  if (method == "DEG") {
+    res <- get_list_of_DEGs(counts_csv_file, 
+                            xp_design_csv_file, 
+                            trtm, 
+                            ref_treatment, 
+                            treatment2, 
+                            log2FC_threshold, 
+                            padj_threshold) %>% rownames_to_column("gene")
+  } else if (method == "LRTcompare") {
+    res <- compare_wald_vs_LRT(trtm = trtm, ref_treatment = ref_treatment, treatment2 = treatment2)
+  } else { 
+    print("no valid method") }
   
-  res <- get_list_of_DEGs(counts_csv_file, 
-                        xp_design_csv_file, 
-                        trtm, 
-                        ref_treatment, 
-                        treatment2, 
-                        log2FC_threshold, 
-                        padj_threshold) %>% rownames_to_column("genes")
   
-  subset_annotated <- biomartr::biomart(genes = res$genes,
+  subset_annotated <- biomartr::biomart(genes = res$gene,
                                         mart = "plants_mart",                 
                                         dataset = "athaliana_eg_gene",           
                                         attributes = attr,        
                                         filters =  "ensembl_gene_id" )
-  names(subset_annotated)[names(subset_annotated) == 'ensembl_gene_id'] <- 'genes'
-  res_annotated <- left_join(res, subset_annotated, by = "genes")
+  names(subset_annotated)[names(subset_annotated) == 'ensembl_gene_id'] <- 'gene'
+  res_annotated <- left_join(res, subset_annotated, by = "gene")
   write_delim(x = res_annotated,
               file = name,
               delim = ";")
