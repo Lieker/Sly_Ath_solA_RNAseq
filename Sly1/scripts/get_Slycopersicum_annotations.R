6790d3f95b4d472a079c1ba0f7d9d3b601bfb24c
lieker <l.e.vlaar@uva.nl>
2021-05-07 07:28:47 +0200

    more analysis Sly1 DEGs

--- Sly1/scripts/get_Slycopersicum_annotations.R
+++ Sly1/scripts/get_Slycopersicum_annotations.R
@@ -1,3 +1,9 @@
+library(biomartr)
+library(tidyverse)
+library("biomaRt")
+library(org.At.tair.db)
+library(stringr)
+
 get_Slycopersicum_annotations <- function(attr = c("description",
                                                    "athaliana_eg_homolog_ensembl_gene",
                                                    "athaliana_eg_homolog_associated_gene_name",
@@ -12,7 +18,7 @@ get_Slycopersicum_annotations <- function(attr = c("description",
                                                       "athaliana_eg_homolog_perc_id")
   ) {
     all_slyc_genes <- GenomicFeatures::makeTxDbFromGFF("Sly1/input/ITAG3.2_gene_models.gff", format = "gff3")
-    all_slyc_genes <- genes(all_slyc_genes) %>% as.data.frame()
+    all_slyc_genes <- GenomicFeatures::genes(all_slyc_genes) %>% as.data.frame()
     
     all_slyc_genes_annotated <- biomartr::biomart(genes = all_slyc_genes$gene_id,
                                                   mart = "plants_mart",                 
